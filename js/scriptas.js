9278fb29f7a54e295f41152082c3cfd3c1519817
Gr0Mk <mjancarasgraphics@gmail.com>
2022-12-12 15:32:20 +0200

    mano pirmasis komitas

---
+++ js/scriptas.js
@@ -0,0 +1,19 @@
+let metai = 1993
+metai = 1993 - 1990
+let menuo = 5
+let diena = 25
+diena = 25 - 20
+var suma = metai + menuo + diena
+console.log(suma)
+
+let a = 1;
+console.log('Kintamojo a reksme: ' + a)
+a = 2;
+console.log('Kintamojo reiksme: ' + a);
+
+let vardasPavarde = 'Martynas Jancaras';
+console.log('Mano vardas ir pavarde: ' +vardasPavarde);
+
+
+
+
