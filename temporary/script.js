693ee830dd2cded90ce9f17690ce536fc57b1916
Gr0Mk <mjancarasgraphics@gmail.com>
2022-12-13 22:00:07 +0200

    Baigiau JavaScript užduotis

---
+++ temporary/script.js
@@ -0,0 +1,17 @@
+let radius = 15;
+let Aplotas =Math.PI * Math.pow(radius, 2);
+let Ailgis = 2 * Math.PI * radius;
+let vardas = 'Vardenis';
+let pavarde = 'Pavardenis';
+let kursas = 1;
+let mokykla = 'CodeAcademy';
+let pmasyvas = [6, 7, 9];
+
+document.write(typeof vardas);
+document.write(typeof pavarde);
+document.write(typeof kursas);
+document.write(typeof mokykla);
+document.write(typeof pmasyvas);
+
+
+
