9b6005bd820e6bc78952e5bd25274a7ede0b8a5e
Gr0Mk <mjancarasgraphics@gmail.com>
2022-12-19 01:04:58 +0200

    JS funkcijos uzduotys

---
+++ temporary/Uzduotys/js/script.js
@@ -0,0 +1,26 @@
+/*TEST*/
+/*
+let auto=[
+    {vardas:'vardas'}
+]
+let taspats="Labas,as"+dmasyvas[0];
+let kitas=`Labas,as ${obj.vardas}`;
+let str=`cia yra stringas`;
+let kitassrt='cia kitas';
+let dv=`dar vienas   zodis kabutese \n"nebepjaunas"  `   ;
+
+let elements = document.getElementById("1task").textContent = dv;
+
+Object.keys(obj).map((key)=>{
+    console.log(obj[key])
+})*/
+/*TEST*/
+
+let dmasyvas = ['Vardenis', 'Pavardenis', 1,'CodeAcademy', [6, 7, 9, 5, 8]];
+document.getElementById("1task").innerHTML = dmasyvas;
+
+
+
+
+
+
